f1ae136a406b98ba466c3029d5bec229a2ff8bcf
Cisino Junior <cisinojr@gmail.com>
2019-10-06 15:31:36 -0300

    Creates responsive menu

---
+++ src/app/main-nav/main-nav.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.scss']
+})
+export class MainNavComponent implements OnInit {
+
+
+  title: string;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.title = environment.appName;
+  }
+
+}
